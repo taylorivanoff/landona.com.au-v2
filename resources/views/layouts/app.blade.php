1c5234d3d27196b7acea66047f4ecdff0b932940
Taylor Ivanoff <taylor.ivanoff@vocus.com.au>
2023-11-09 11:54:19 +1100

    Added GA4.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,55 +1,59 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name') }}</title>
+    <title>{{ config('app.name') }}</title>
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-        <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
 
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
-        <!-- Google tag (gtag.js) -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-BDS0LE2JFE"></script>
-        <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
+    <!-- Google tag (gtag.js) -->
+    <script async src=https://www.googletagmanager.com/gtag/js?id=G-ZXLK437JYZ></script>
+    <script> window.dataLayer = window.dataLayer || [];
 
-            gtag('config', 'G-BDS0LE2JFE');
-        </script>
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen">
-            @include('layouts.navigation')
+        function gtag() {
+            dataLayer.push(arguments);
+        }
 
-            <!-- Page Heading -->
-            @isset ($header)
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
+        gtag('js', new Date());
+        gtag('config', 'G-ZXLK437JYZ');
+    </script>
+</head>
+<body class="font-sans antialiased">
+<div class="min-h-screen">
+    @include('layouts.navigation')
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+    <!-- Page Heading -->
+    @isset ($header)
+        <header class="bg-white shadow">
+            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                {{ $header }}
+            </div>
+        </header>
+    @endisset
 
-            <!-- Page Footer -->
-            <footer class="bg-white text-gray-500">
-                <div class="mx-auto px-4 sm:px-6 lg:px-8 py-10 tracking-tight">
-                    <div class="max-w-6xl mx-auto">
-                        <div class="text-xs align-middle inline-block">©{{ now()->year }} Landona Conveyancing Pty. Ltd. License No.: 05006589</div>
-                    </div>
+    <!-- Page Content -->
+    <main>
+        {{ $slot }}
+    </main>
+
+    <!-- Page Footer -->
+    <footer class="bg-white text-gray-500">
+        <div class="mx-auto px-4 sm:px-6 lg:px-8 py-10 tracking-tight">
+            <div class="max-w-6xl mx-auto">
+                <div class="text-xs align-middle inline-block">©{{ now()->year }} Landona Conveyancing Pty. Ltd. License
+                    No.: 05006589
                 </div>
-            </footer>
+            </div>
         </div>
-    </body>
+    </footer>
+</div>
+</body>
 </html>
