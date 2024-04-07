75dd28842cdf2e0739d128cffdf9ad0c738c3e83
Taylor Ivanoff <taylorivanoff@gmail.com>
2024-04-07 19:54:49 +1000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,5 @@ cd ~/landona.com.au
 # Pull the latest changes from your GitHub repository
 git pull origin main
 
-# Run npm build to build your project
-npm install         # Install dependencies if needed
-npm run prod       # Run the build command
-
 # Build your Docker containers
 docker-compose up -d --build
