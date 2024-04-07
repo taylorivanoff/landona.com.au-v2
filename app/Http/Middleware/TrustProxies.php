8dc05dc148d1291e0a9c444bb6b35844b2955b50
Taylor Ivanoff <taylor.ivanoff@vocus.com.au>
2024-04-07 18:54:41 +1000

    Changed site confs.

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,9 @@ class TrustProxies extends Middleware
      *
      * @var array|string|null
      */
-    protected $proxies;
+    protected $proxies = [
+        '172.17.0.1'
+    ];
 
     /**
      * The headers that should be used to detect proxies.
