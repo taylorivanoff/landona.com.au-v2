ff80e3817c9960d8c8664be5d516f694cd1e9959
Taylor Ivanoff <taylor.ivanoff@vocus.com.au>
2024-06-03 22:56:31 +1000

    UI changes

---
+++ resources/views/parts/pexa.blade.php
@@ -0,0 +1,13 @@
+<x-section id="from-contract-to-settlement-pexa">
+    <x-h type="h2">
+        From Contract to Settlement
+    </x-h>
+    <x-p>
+        With 20 years of NSW conveyancing experience and secure digital settlements via PEXA, we ensure
+        you're protected at every step of the way.
+    </x-p>
+    <x-content-button href="https://www.pexa.com.au/solutions/buyers-sellers/#overview" target="_blank"
+                      id="services">
+        Find Out More ➜
+    </x-content-button>
+</x-section>
---
+++ resources/views/parts/testimonials.blade.php
@@ -0,0 +1,19 @@
+@include('parts/reviews')
+
+<x-section id="testimonials">
+    <x-h type="h2">
+        Over 2,000 Properties Settled
+    </x-h>
+    <h2 class="font-serif text-3xl md:text-4xl text-gray-900 leading-relaxed">
+
+    </h2>
+    <x-p class="lg:text-lg ">
+        Join our satisfied clients who have entrusted us to handle their
+        property matters with professionalism and care.
+    </x-p>
+    <div>
+        <iframe
+            src="https://www.google.com/maps/d/embed?mid=1wiSfjrr_Hu3UYsi3RmKo59bNsIkCGpuP&ehbc=2E312F"
+            class="w-full" height="520"></iframe>
+    </div>
+</x-section>
