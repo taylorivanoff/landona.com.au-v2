8c7dcb2fced50eb9d7a19ce5825f32228e061fa1
Taylor Ivanoff <taylor.ivanoff@vocus.com.au>
2024-06-04 00:22:31 +1000

    UI changes

--- resources/views/components/ul.blade.php
+++ resources/views/components/ul.blade.php
@@ -1,3 +1,3 @@
-<ul class="list-disc ml-8 space-y-4 text-gray-600 text-left {{ $attributes->get('class') }}">
+<ul class="list-disc ml-8 space-y-4 text-gray-600 text-left flex space-x-2 {{ $attributes->get('class') }}">
     {{$slot}}
 </ul>
--- resources/views/pages/resources.blade.php
+++ resources/views/pages/resources.blade.php
@@ -3,21 +3,21 @@
         <x-h type="h1">Resources</x-h>
         <h3 class="font-semibold">Guides and Checklists</h3>
         <x-ul>
-            <div class="flex space-x-2">
-                <li>
-                    <a href="https://www.revenue.nsw.gov.au/grants-schemes/first-home-buyer" target="_blank" class="underline">First Homebuyer’s Guide</a>
-                </li>
-                <x-icon-external-link/>
-            </div>
+            <li>
+                <a href="https://www.revenue.nsw.gov.au/grants-schemes/first-home-buyer" target="_blank"
+                   class="underline">First Homebuyer’s Guide
+                    <x-icon-external-link/>
+                </a>
+            </li>
         </x-ul>
         <h3 class="font-semibold">Tools</h3>
         <x-ul>
-            <div class="flex space-x-2">
-                <li>
-                    <a href="https://www.apps09.revenue.nsw.gov.au/erevenue/calculators/landsalesimple.php" target="_blank" class="underline">Stamp Duty Calculator</a>
-                </li>
-                <x-icon-external-link/>
-            </div>
+            <li>
+                <a href="https://www.apps09.revenue.nsw.gov.au/erevenue/calculators/landsalesimple.php" target="_blank"
+                   class="underline">Stamp Duty Calculator
+                    <x-icon-external-link/>
+                </a>
+            </li>
         </x-ul>
     </x-section-full>
 </x-app-layout>
