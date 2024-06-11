7a8dcdc0d59b1e9709a392ca2da51519a9d9678c
Taylor Ivanoff <taylor.ivanoff@vocus.com.au>
2024-06-12 03:02:35 +1000

    Added mailgun

--- app/Console/Commands/GenerateSitemap.php
+++ app/Console/Commands/GenerateSitemap.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Spatie\Crawler\Url;
 use Spatie\Sitemap\SitemapGenerator;
 
 class GenerateSitemap extends Command
@@ -28,11 +29,23 @@ class GenerateSitemap extends Command
      */
     public function handle()
     {
+        // The Crawler
         SitemapGenerator::create(config('app.url'))
+            ->hasCrawled(function (Url $url) {
+                // Exclude URLs that contain 'admin'
+                if (str_contains($url->url, 'admin')) {
+                    return;
+                }
+
+                // Set a custom priority for certain URLs
+//                if ($url->url === config('app.url') . '/special-page') {
+//                    $url->setPriority(1.0);
+//                }
+
+                // Add the last modified date
+                $url->setLastModificationDate(now());
+
+                return $url;
+            })
             ->writeToFile(public_path('sitemap.xml'));
-
-        $this->info('Sitemap generated successfully.');
-
-        return Command::SUCCESS;
-    }
-}
+    }}
