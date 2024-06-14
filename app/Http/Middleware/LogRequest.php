08142b13d83e0881f2af592bf9b037377f1bd349
Taylor Ivanoff <taylor.ivanoff@vocus.com.au>
2024-06-15 02:33:42 +1000

    Added page view and unique visitor stats.

--- app/Http/Middleware/LogRequest.php
+++ app/Http/Middleware/LogRequest.php
@@ -6,6 +6,7 @@ use App\Models\Event;
 use Closure;
 use Error;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 use Symfony\Component\HttpFoundation\Response;
 
 class LogRequest
--- routes/web.php
+++ routes/web.php
@@ -16,15 +16,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [IndexController::class, 'index'])->name('home');
-Route::get('/resources', fn() => view('pages/home/resources'))->name('resources');
-Route::get('/about-us', fn() => view('pages/home/about-us'))->name('about-us');
-Route::get('/enquiry', [EnquiryController::class, 'create'])->name('enquiries.create');
-Route::post('/enquiry', [EnquiryController::class, 'store'])->name('enquiries.store');
+require __DIR__ . '/auth.php';
 
-// Auth
-Route::get('/dashboard', fn() => view('pages/dashboard/dashboard'))->middleware(['auth'])->name('dashboard');
-Route::get('/admin/events', [EventController::class, 'index'])->name('admin.events');
-Route::get('/admin/events/data', [EventController::class, 'data'])->name('admin.events.data');
+Route::middleware('guest')->group(function () {
+    Route::get('/', [IndexController::class, 'index'])->name('home');
+    Route::get('/resources', fn() => view('pages/home/resources'))->name('resources');
+    Route::get('/about-us', fn() => view('pages/home/about-us'))->name('about-us');
+    Route::get('/enquiry', [EnquiryController::class, 'create'])->name('enquiries.create');
+    Route::post('/enquiry', [EnquiryController::class, 'store'])->name('enquiries.store');
+});
 
-require __DIR__.'/auth.php';
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', fn() => view('pages/dashboard/dashboard'))->middleware(['auth'])->name('dashboard');
+    Route::get('/admin/events', [EventController::class, 'index'])->name('admin.events');
+    Route::get('/admin/events/data', [EventController::class, 'data'])->name('admin.events.data');
+});
+
